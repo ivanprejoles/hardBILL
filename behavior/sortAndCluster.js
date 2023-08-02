8dfab0948e92ebf82501632cab83083fe73de8e3
ivanprejoles <100924087+ivanprejoles@users.noreply.github.com>
2023-08-02 15:53:50 +0800

    Update sortAndCluster.js

--- behavior/sortAndCluster.js
+++ behavior/sortAndCluster.js
@@ -291,11 +291,9 @@ function sortNestedArrays(arr, indices) {
       let stored = JSON.parse(storedData);
       let sort = stored.sortingReference;
       for(let i = 0; i < sort.length; i++){
-        console.log('dsadsa')
         data.sortAndCluster.pickedIndex = sort[i];
         addSort();
       }
-        console.log('22')
     }
   }
   makeAddSort()
